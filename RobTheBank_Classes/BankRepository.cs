77196be8582d0081e1bede43256093d50765de72
AThooker <austintrent@icloud.com>
2020-07-09 16:44:12 -0400

    rob the bank

---
+++ RobTheBank_Classes/BankRepository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Pipes;
+using System.Linq;
+using System.Net.Mime;
+using System.Text;
+using System.Threading.Tasks;
+namespace RobTheBank_Classes
+{
+    public class BankRepository
+    {
+        List<Questions> _questionList = new List<Questions>();
+        List<Responses> _responseList = new List<Responses>();
+
+        public void AddQuestionsToList(Questions content)
+        {
+            _questionList.Add(content);
+        }
+
+        public void AddResponseToList(Responses content)
+        {
+            _responseList.Add(content);
+        }
+
+        public Questions GetQuestionByNumber(int number)
+        {
+          foreach(Questions content in _questionList)
+            {
+                if(content.Question == number)
+                {
+                    return content;
+                }
+            }
+            return null;
+        }
+
+        public Responses GetResponsesByNumber(string selection)
+        {
+            foreach (Responses content in _responseList)
+            {
+                if (content.Answer == selection)
+                {
+                    return content;
+                }
+            }
+            return null;
+        }
+
+    }
+}
